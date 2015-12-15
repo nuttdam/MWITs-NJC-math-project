16c411f084f9e713a90f6e0754edee349885e521
nuttdam <i@nuttdam.com>
2015-12-15 22:47:08 +0700

    reduce line count in output

--- code.py
+++ code.py
@@ -87,13 +87,17 @@ timer = 0
 node_counter = 0
 steps = 50 # this is the number of steps until program terminates
 while(timer < steps):
-    print("-----")
+    '''
+	print("-----")
     file.write("-----\n")
     print("step: " + str(timer))
     file.write("step: "+str(timer)+"\n")
+	'''
     node_counter = node_counter + n(timer)
+	'''
     print("node_counter: " + str(node_counter))
     file.write("node_counter: " + str(node_counter) +"\n")
+	'''
     update_state()
     betweenness(g=g,vprop=v_betweenness)
     sum_betweenness = 0
@@ -101,8 +105,8 @@ while(timer < steps):
         sum_betweenness += v_betweenness.a[v]
     if(node_counter != 0):
         sum_betweenness /= node_counter
-    print("betweenness centrality= " + str(sum_betweenness))
-    file.write("betweenness centrality= " + str(sum_betweenness) + "\n")
+    print(str(timer) + ": " + str(sum_betweenness))
+    file.write(str(timer) + ": " + str(sum_betweenness) + "::\n\n")
     timer += 1
 	
 file.close()
